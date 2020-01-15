d510152325780fb031854bc072a095075fcdb9a3
Ana Nikolova <ani.nik19@gmail.com>
2020-01-15 09:40:01 +0100

    moving the html and displaying the movies

--- index.js
+++ index.js
@@ -5,24 +5,42 @@ const fetchData = async searchTerm => {
       s: searchTerm
     }
   });
+  if (response.data.Error) {
+    return [];
+  }
 
   return response.data.Search;
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<lable><b>Serach for a movie</b></lable>
+<input  class="input"/>
+  <div class="dropdown ">
+      <div class="dropdow-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
-  console.log(movies);
+
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
-    const div = document.createElement("div");
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
 
-    div.innerHTML = `
-    <img src="${movie.Poster}" />
-     <h1>${movie.Title}</h1>`;
-   
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+    <img src="${imgSrc}" />
+     ${movie.Title}`;
 
-    document.querySelector("#target").appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 
