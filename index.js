aeb4c9e44013767c3c187594c00eb83d576a76e8
Ana Nikolova <ani.nik19@gmail.com>
2020-01-21 07:34:35 +0100

    parsing number of Awards

--- index.js
+++ index.js
@@ -90,8 +90,28 @@ const movieTemplate = movieDetail => {
   const metascore = parseInt(movieDetail.Metascore);
   const imdbRating = parseInt(movieDetail.imdbRating);
   const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""));
+  // var count = 0;
+  // const awards = movieDetail.Awards.split(" ").forEach(word => {
+  //   const value = parseInt(word);
 
-  console.log(metascore, imdbRating, imdbVotes);
+  //   if (isNaN(value)) {
+  //     return;
+  //   } else {
+  //     count = count + value;
+  //   }
+  // });
+
+  const awards = movieDetail.Awards.split(" ").reduce((prev, word) => {
+    const value = parseInt(word);
+
+    if (isNaN(value)) {
+      return prev;
+    } else {
+      return prev + value;
+    }
+  }, 0);
+
+  console.log(awards);
 
   return `
     <article class="media">
